5aced98e037d26ab8a83cc2f354538bdd5faca17
Ahmed Mostafa <ahmedmostafa22sh@gmail.com>
2025-09-21 20:04:51 +0300

    finish arch

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,10 @@ void main() {
 class DoctorApp extends StatelessWidget {
   const DoctorApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
+
       home: const Scaffold(),
     );
   }
