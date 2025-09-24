c8345451f1cecd19d18a9d38a2414612caf3986e
Ahmed Mostafa <ahmedmostafa22sh@gmail.com>
2025-09-25 01:38:34 +0300

    handle some bugs

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,12 @@ import 'package:booking_doctor/core/constants/app_routes.dart';
 import 'package:booking_doctor/core/utils/app_routers.dart';
 import 'package:booking_doctor/features/booking_appointment/domain/payment/stripe_keys.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
 
-void main() {
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName:"keys.env");
   Stripe.publishableKey=StripeKeys.publishableKey;
   runApp(const DoctorApp());
 }
